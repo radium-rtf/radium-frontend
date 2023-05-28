e7f453b06d4c7be46e14ed2ec43a1fcb8e3fccd8
rustam41k0 <rustamskysan@gmail.com>
2023-05-28 13:48:43 +0500

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,37 @@
-import { CSSProperties, FC } from "react"
+import {CSSProperties, FC} from "react"
 import styles from './Header.module.scss';
 import logo from "../../images/kotlin.svg"
+import muscleman from "../../images/кач.jpg"
 
 interface IHeaderProps {
     title?: string;
     className?: string;
     style?: CSSProperties;
+    caption?: string;
+    userLogin?: string;
 }
 
 const Header: FC<IHeaderProps> = ({
-      title='Title',
-      className='',
-      style
-}) => {
+      title = 'Title',
+      className = '',
+      style,
+      caption = '',
+      userLogin = '',
+  }) => {
     return (
         <div className={styles[className]}>
-            <ul>
-                <li><img src={logo} alt="картинка курса"/></li>
-                <li><b>{title}</b></li>
-            </ul>
-
-
-            {/*<div className={styles['courseHeaders']}>*/}
-            {/*</div>*/}
+            <div className={styles["caption"]}>
+                <b>{caption}</b>
+            </div>
+            <div className={styles["title"]}>
+                <img src={logo} alt="картинка курса"/>
+                <b>{title}</b>
+            </div>
+            <div className={styles["profile"]}>
+                <p>{userLogin}</p>
+                <img src={muscleman} alt=""/>
+            </div>
         </div>
-    )
+)
 }
 export default Header;
