be8641090b2735034510062901a917b83b718e6c
rustam41k0 <rustamskysan@gmail.com>
2023-05-28 19:15:09 +0500

    complete menu component

--- src/interfaces/menu.interface.ts
+++ src/interfaces/menu.interface.ts
@@ -1,19 +1,27 @@
+import profile from "../images/profile.svg"
+import myCourses from "../images/courses.svg"
+import exit from "../images/exit.svg"
+
 export interface IMenu {
     label: string,
     path: string,
+    image: string,
 }
 
 export const menus: IMenu[] = [
     {
         label: 'Профиль',
-        path: '/profile'
+        path: '/profile',
+        image: profile,
     },
     {
         label: 'Мои курсы',
-        path: '/mycourses'
+        path: '/mycourses',
+        image: myCourses,
     },
     {
         label: 'Выйти',
-        path: '/logout'
+        path: '/logout',
+        image: exit,
     }
 ]
