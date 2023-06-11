b95488048b80de9f6cde0a681904ac463eb0190c
olgnite <lickotynf2@yandex.ru>
2023-06-11 15:38:48 +0300

    ура можно проходить курс (пока на моках)

--- src/components/Section/SectionCard.tsx
+++ src/components/Section/SectionCard.tsx
@@ -1,10 +1,12 @@
-import {Section} from "../../interfaces/module.interface";
-import {FC, useState} from "react";
-import {SubmitHandler, useForm} from "react-hook-form";
+import { Section } from "../../interfaces/module.interface";
+import { FC, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import TextSection from "../TextSection/TextSection";
 import ShortAnswerSection from "../ShortAnswerSection/ShortAnswerSection";
 import SingleChoiceSection from "../SingleChoiceSection/SingleChoiceSection";
 import MultiChoiceSection from "../MultiChoiceSection/MultiChoiceSection";
+import { TypeAnswer, getTypeAnswer } from "../../utils/utils";
+
 
 const SectionCard: FC<Section> = ({
     id,
@@ -15,43 +17,42 @@ const SectionCard: FC<Section> = ({
     choice,
     multichoice,
 }) => {
-    const [state, setState] = useState<"initial" | "correct" | "partial" | "incorrect">("initial");
+    const [state, setState] = useState<TypeAnswer>('initial');
     const { handleSubmit, register } = useForm<{ answer: string }>();
 
     const answerHandler: SubmitHandler<{ answer: string }> = ({ answer }) => {
-        // TODO: ответы
-        setState("correct");
+        setState(getTypeAnswer(answer));
     }
 
     return text
         ? <TextSection>{text.content}</TextSection>
-    : shortanswer
-        ? <ShortAnswerSection
-            register={() => register('answer')}
-            onSubmit={() => handleSubmit(answerHandler)}
-            question={shortanswer.question}
-            maxScore={shortanswer.maxScore}
-            state={state}
-        />
-    : choice
-        ? <SingleChoiceSection
-            register={() => register('answer')}
-            onSubmit={() => handleSubmit(answerHandler)}
-            question={choice.question}
-            choices={choice.variants}
-            maxScore={choice.maxScore}
-            state={state}
-        />
-    : multichoice
-        ? <MultiChoiceSection
-            register={() => register('answer')}
-            onSubmit={() => handleSubmit(answerHandler)}
-            question={multichoice.question}
-            choices={multichoice.variants}
-            maxScore={multichoice.maxScore}
-            state={state}
-        />
-        : <></>
+        : shortanswer
+            ? <ShortAnswerSection
+                register={() => register('answer')}
+                onSubmit={handleSubmit(answerHandler)}
+                question={shortanswer.question}
+                maxScore={shortanswer.maxScore}
+                state={state}
+            />
+            : choice
+                ? <SingleChoiceSection
+                    register={() => register('answer')}
+                    onSubmit={handleSubmit(answerHandler)}
+                    question={choice.question}
+                    choices={choice.variants}
+                    maxScore={choice.maxScore}
+                    state={state}
+                />
+                : multichoice
+                    ? <MultiChoiceSection
+                        register={() => register('answer')}
+                        onSubmit={handleSubmit(answerHandler)}
+                        question={multichoice.question}
+                        choices={multichoice.variants}
+                        maxScore={multichoice.maxScore}
+                        state={state}
+                    />
+                    : <></>
 }
 
 export default SectionCard
