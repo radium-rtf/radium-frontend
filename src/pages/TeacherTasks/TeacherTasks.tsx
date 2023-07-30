36f66c87acbb6b892ee8525d39be96910548bab3
rustam41k0 <rustamskysan@gmail.com>
2023-07-30 21:03:23 +0400

    доработал страницу с заданиями

--- src/pages/TeacherTasks/TeacherTasks.tsx
+++ src/pages/TeacherTasks/TeacherTasks.tsx
@@ -8,7 +8,30 @@ import ava from "../../images/кач.jpg";
 import styles from './TeacherTasks.module.scss';
 import CourseCard from "../../components/CourseCard/CourseCard";
 
-const tasks = [
+const checkedTasks = [
+    {
+        id: 1,
+        name: 'Основы программирования на Kotlin',
+        logo: kotlinLogo,
+        title: 'Классы данных',
+        subtitle: 'Классы и наследование',
+        profileImage: ava,
+        checkerName: 'Алехандро',
+        comment: '*комментарий*',
+    },
+    {
+        id: 2,
+        name: 'Основы программирования на Kotlin',
+        logo: kotlinLogo,
+        title: 'Классы данных',
+        subtitle: 'Классы и наследование',
+        profileImage: ava,
+        checkerName: 'Алехандро',
+        comment: '*комментарий*',
+    },
+]
+
+const uncheckedTasks = [
     {
         id: 1,
         name: 'Основы программирования на Kotlin',
@@ -45,33 +68,59 @@ const TeacherTasks: FC = () => {
             <Background />
             <TopPanel
                 image={kotlinLogo}
-                title='Основы программирования на котлин'
+                title='Основы программирования на Kotlin'
                 username={name}
                 profile={avatar}
             />
             <div className={styles.wrapper}>
-                <div className={styles.teacherTasksHeader}>
-                    <h2>Текущие курсы</h2>
-                    <div className={styles.teacherTasks}>
-                        {tasks.map((task) =>
-                            <CourseCard
-                                key={task.id}
-                                name={task.name}
-                                image={task.logo}
-                                title={task.title}
-                                subtitle={task.subtitle}
-                                checkerName={task.checkerName}
-                                profileImage={task.profileImage}
-                                comment={task.comment}
-                                progress={0.34}
-                                topic='Следующая тема'
-                                state='done'
-                                button='full'
-                                width={480}
-                            />)}
+                <div>
+                    <div className={styles.teacherTasksHeader}>
+                        <h2>Ожидает проверки</h2>
+                        <div className={styles.teacherTasks}>
+                            {checkedTasks.map((task) =>
+                                <CourseCard
+                                    key={task.id}
+                                    name={task.name}
+                                    image={task.logo}
+                                    title={task.title}
+                                    subtitle={task.subtitle}
+                                    checkerName={task.checkerName}
+                                    profileImage={task.profileImage}
+                                    comment={task.comment}
+                                    progress={0.34}
+                                    topic='Следующая тема'
+                                    state='done'
+                                    button='full'
+                                    width={480}
+                                />)}
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.teacherTasksHeader2}>
+                        <h2>Проверено</h2>
+                        <div className={styles.teacherTasks}>
+                            {uncheckedTasks.map((task) =>
+                                <CourseCard
+                                    key={task.id}
+                                    name={task.name}
+                                    image={task.logo}
+                                    title={task.title}
+                                    subtitle={task.subtitle}
+                                    checkerName={task.checkerName}
+                                    profileImage={task.profileImage}
+                                    comment={task.comment}
+                                    progress={0.34}
+                                    topic='Следующая тема'
+                                    state='checked'
+                                    button='full'
+                                    width={480}
+                                />)}
+                        </div>
                     </div>
                 </div>
             </div>
+
         </>
     );
 };
