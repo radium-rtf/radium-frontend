b04ccda8664e3e807a6d8bf2ef73632e861bdfc2
arista213 <v.karpov@skbkontur.ru>
2023-09-10 15:25:35 +0500

    fix: fix TextArea

--- src/shared/ui/TextArea.tsx
+++ src/shared/ui/TextArea.tsx
@@ -1,10 +1,9 @@
-import React, { ChangeEventHandler } from "react";
-import { FC } from "react";
+'use client'
+import { ChangeEventHandler, FC, useState } from "react";
 import { cn } from "../utils/cn";
 
 interface TextAreaProps {
     defaultValue?: string,
-    hasText: boolean,
     className?: string,
     onChange?: ChangeEventHandler<HTMLTextAreaElement>,
     disabled?: boolean,
@@ -12,23 +11,26 @@ interface TextAreaProps {
 
 export const TextArea: FC<TextAreaProps> = ({
     defaultValue,
-    hasText,
     className,
     onChange,
-    disabled }) => (
-    <textarea
-        onChange={onChange}
+    disabled }) => {
+    const [hasText, setHasText] = useState(false);
+
+    return <textarea
+        onChange={e => {
+            setHasText(true);
+            onChange?.(e);
+        }}
         disabled={disabled}
         defaultValue={defaultValue}
         className={cn(
             className,
             "p-4 w-64 h-32 bg-green-300 border-grey-300 bg-transparent leading-tight border rounded-lg resize-none text-text-secondary outline-none",
-            { "text-text-primary": hasText },
             "disabled:cursor-not-allowed disabled:opacity-50",
             "hover:bg-white hover: hover:bg-opacity-5",
             "active:bg-black active:focus-visible:bg-opacity-10 active:outline-none",
             "focus-visible:outline-1 focus-visible:outline-white focus-visible:outline-offset-0",
-            "focus-visible:bg-white hover: focus-visible:bg-opacity-5"
-        )}
-    />
-);
+            "focus-visible:bg-white hover: focus-visible:bg-opacity-5",
+            { "text-text-primary": hasText }
+        )} />;
+};
