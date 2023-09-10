38fa6bc095442d40686644b9a31a9b79f0b0fe3f
Danila Petrov <dp200308@gmail.com>
2023-09-10 14:13:44 +0500

    feat: add icon component

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,3 +5,4 @@ export { Radio } from './ui/Radio';
 export { Button } from './ui/Button';
 export { Toggle } from './ui/Toggle';
 export { Checkbox } from './ui/Checkbox';
+export { Icon } from './ui/Icon';
