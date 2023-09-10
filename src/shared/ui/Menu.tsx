5cd33aeec23f898acb3d17774b57e6d02d32b3a0
Danila Petrov <dp200308@gmail.com>
2023-09-10 19:53:26 +0500

    fix: menu background color fix

--- src/shared/ui/Menu.tsx
+++ src/shared/ui/Menu.tsx
@@ -4,12 +4,11 @@ import { IMenuItem } from './MenuItem';
 
 interface IProps {
   children?: ReactElement<IMenuItem> | ReactElement<IMenuItem>[];
-  onClose?: () => void;
 }
 
-export const Menu: FC<IProps> = ({ children, onClose }) => {
+export const Menu: FC<IProps> = ({ children }) => {
   return (
-    <ul className='text-text-primary [&>li:first-child>button]:rounded-t [&>li:last-child>button]:rounded-b [&>li>button]:rounded-none'>
+    <ul className='rounded border border-grey-300 bg-grey-500 text-text-primary [&>li:first-child>button]:rounded-t [&>li:last-child>button]:rounded-b [&>li>button]:rounded-none'>
       {children}
     </ul>
   );
