0efb261947f9e9990bc765db32c3de22fa4f31e8
Danila Petrov <dp200308@gmail.com>
2023-09-10 20:30:14 +0500

    fix: fixed menu styles

--- src/shared/ui/Menu.tsx
+++ src/shared/ui/Menu.tsx
@@ -1,14 +1,30 @@
 'use client';
 import { FC, ReactElement } from 'react';
 import { IMenuItem } from './MenuItem';
+import { cn } from '@/shared';
 
 interface IProps {
   children?: ReactElement<IMenuItem> | ReactElement<IMenuItem>[];
+  className?: string;
 }
 
-export const Menu: FC<IProps> = ({ children }) => {
+export const Menu: FC<IProps> = ({ children, className }) => {
   return (
-    <ul className='rounded border border-grey-300 bg-grey-500 text-text-primary [&>li:first-child>button]:rounded-t [&>li:last-child>button]:rounded-b [&>li>button]:rounded-none'>
+    <ul
+      className={cn(
+        [
+          'rounded',
+          'border',
+          'border-grey-300',
+          'bg-grey-500',
+          'text-text-primary',
+          '[&>li:first-child>button]:rounded-t',
+          '[&>li:last-child>button]:rounded-b',
+          '[&>li>button]:rounded-none',
+        ],
+        className
+      )}
+    >
       {children}
     </ul>
   );
