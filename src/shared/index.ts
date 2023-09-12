f7487aa334d9b2fcda415a6078060eda4f748bf7
Danila Petrov <dp200308@gmail.com>
2023-09-12 13:29:07 +0500

    refactor: removed state hook from textarea component

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,3 +5,4 @@ export { Radio } from './ui/Radio';
 export { Button } from './ui/Button';
 export { Toggle } from './ui/Toggle';
 export { Checkbox } from './ui/Checkbox';
+export { TextArea } from './ui/TextArea';
--- src/shared/ui/TextArea.tsx
+++ src/shared/ui/TextArea.tsx
@@ -1,36 +1,56 @@
-'use client'
-import { ChangeEventHandler, FC, useState } from "react";
-import { cn } from "../utils/cn";
+'use client';
+import { ChangeEventHandler, FC } from 'react';
+import { cn } from '@/shared';
 
 interface TextAreaProps {
-    defaultValue?: string,
-    className?: string,
-    onChange?: ChangeEventHandler<HTMLTextAreaElement>,
-    disabled?: boolean,
+  defaultValue?: string;
+  className?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+  disabled?: boolean;
+  placeholder?: string;
 }
 
 export const TextArea: FC<TextAreaProps> = ({
-    defaultValue,
-    className,
-    onChange,
-    disabled }) => {
-    const [hasText, setHasText] = useState(false);
-
-    return <textarea
-        onChange={e => {
-            setHasText(true);
-            onChange?.(e);
-        }}
-        disabled={disabled}
-        defaultValue={defaultValue}
-        className={cn(
-            className,
-            "p-4 w-64 h-32 bg-green-300 border-grey-300 bg-transparent leading-tight border rounded-lg resize-none text-text-secondary outline-none",
-            "disabled:cursor-not-allowed disabled:opacity-50",
-            "hover:bg-white hover: hover:bg-opacity-5",
-            "active:bg-black active:focus-visible:bg-opacity-10 active:outline-none",
-            "focus-visible:outline-1 focus-visible:outline-white focus-visible:outline-offset-0",
-            "focus-visible:bg-white hover: focus-visible:bg-opacity-5",
-            { "text-text-primary": hasText }
-        )} />;
+  defaultValue,
+  className,
+  onChange,
+  disabled,
+  placeholder,
+}) => {
+  return (
+    <textarea
+      placeholder={placeholder}
+      onChange={onChange}
+      disabled={disabled}
+      defaultValue={defaultValue}
+      className={cn(
+        [
+          'p-4',
+          'text-sm',
+          'rounded',
+          'border',
+          'border-grey-500',
+          'outline-white',
+          '-outline-offset-1',
+          'bg-transparent',
+          'text-text-primary',
+          'resize',
+          'transition-colors',
+          'max-w-full',
+          'placeholder:text-grey-100',
+          'disabled:border-grey-600',
+          'disabled:placeholder:text-grey-300',
+          'disabled:text-grey-300',
+          'disabled:cursor-not-allowed',
+          'disabled:hover:bg-transparent',
+          'hover:bg-grey-600',
+          'hover:placeholder:text-text-primary',
+          'focus:bg-grey-800',
+          'focus:placeholder:text-text-secondary',
+          'focus-visible:outline',
+        ],
+        className
+      )}
+    />
+  );
 };
