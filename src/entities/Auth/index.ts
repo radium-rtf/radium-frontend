b3d6975432b10ff74129bb2fb3df9cb641973fa2
Danila Petrov <dp200308@gmail.com>
2023-09-12 15:51:39 +0500

    feat: add auth data transfer object interfaces

---
+++ src/entities/Auth/index.ts
@@ -0,0 +1,5 @@
+// Interfaces
+export type { LoginRequestDto } from './model/LoginRequestDto';
+export type { AuthResponseDto } from './model/AuthResponseDto';
+export type { SuccessAuthResponseDto } from './model/SuccessAuthResponseDto';
+export type { ErrorAuthResponseDto } from './model/ErrorAuthResponseDto';
