fc0ca5ff233f35fff4122632ccbdef89c575ddbc
Danila Petrov <dp200308@gmail.com>
2023-09-12 15:52:06 +0500

    feat: add auth login method

--- src/entities/Auth/index.ts
+++ src/entities/Auth/index.ts
@@ -3,3 +3,6 @@ export type { LoginRequestDto } from './model/LoginRequestDto';
 export type { AuthResponseDto } from './model/AuthResponseDto';
 export type { SuccessAuthResponseDto } from './model/SuccessAuthResponseDto';
 export type { ErrorAuthResponseDto } from './model/ErrorAuthResponseDto';
+
+// Methods
+export { Login } from './libs/Login';
---
+++ src/entities/Auth/libs/Login.ts
@@ -0,0 +1,14 @@
+import { AuthResponseDto, LoginRequestDto } from '@/entities/Auth';
+
+export const Login = async (
+  credentials: LoginRequestDto
+): Promise<AuthResponseDto> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/auth/signin`,
+    {
+      method: 'POST',
+      cache: 'no-cache',
+    }
+  );
+  return response.json();
+};
