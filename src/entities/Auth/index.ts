916e26e0eac47db5fe5d28a54d5bb8dd9107569f
Danila Petrov <dp200308@gmail.com>
2023-09-12 19:17:10 +0500

    feat: add register method

--- src/entities/Auth/index.ts
+++ src/entities/Auth/index.ts
@@ -1,8 +1,10 @@
 // Interfaces
 export type { LoginRequestDto } from './model/LoginRequestDto';
+export type { RegisterRequestDto } from './model/RegisterRequestDto';
 export type { AuthResponseDto } from './model/AuthResponseDto';
 export type { SuccessAuthResponseDto } from './model/SuccessAuthResponseDto';
 export type { ErrorAuthResponseDto } from './model/ErrorAuthResponseDto';
 
 // Methods
 export { Login } from './libs/Login';
+export { Register } from './libs/Register';
