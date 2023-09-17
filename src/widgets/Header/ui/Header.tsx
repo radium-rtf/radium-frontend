70e15077fbeed8287fef755dbbc1a333944d9574
Danila Petrov <dp200308@gmail.com>
2023-09-17 21:32:20 +0500

    fix: add position sticky for header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -9,10 +9,12 @@ export const Header: FC<IProps> = ({ children }) => {
   return (
     <header
       className='
-        bg-bg-page
+        sticky
+        top-0
         flex
         items-center
         justify-between
+        bg-bg-page
         px-12
         pb-9
         pt-12
