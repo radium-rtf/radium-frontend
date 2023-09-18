82da5c35ca930b8120a96a01c1513a9ebe9851e8
Danila Petrov <dp200308@gmail.com>
2023-09-18 16:14:20 +0500

    feat: add answer section response dto

--- src/entities/Section/index.ts
+++ src/entities/Section/index.ts
@@ -3,6 +3,7 @@ export type { SectionResponseDto } from './model/sectionResponseDto';
 export type { TextSectionResponseDto } from './model/textSectionResponseDto';
 export type { ChoiceSectionResponseDto } from './model/choiceSectionResponseDto';
 export type { MultiChoiceSectionResponseDto } from './model/multiChoiceSectionResponseDto';
+export type { AnswerSectionResponseDto } from './model/answerSectionResponseDto';
 
 // types
 export type { AllSectionsResponseDto } from './model/allSectionsResponseDto';
--- src/entities/Section/model/allSectionsResponseDto.ts
+++ src/entities/Section/model/allSectionsResponseDto.ts
@@ -1,8 +1,10 @@
 import { MultiChoiceSectionResponseDto } from '..';
+import { AnswerSectionResponseDto } from './answerSectionResponseDto';
 import { ChoiceSectionResponseDto } from './choiceSectionResponseDto';
 import { TextSectionResponseDto } from './textSectionResponseDto';
 
 export type AllSectionsResponseDto =
   | TextSectionResponseDto
   | ChoiceSectionResponseDto
-  | MultiChoiceSectionResponseDto;
+  | MultiChoiceSectionResponseDto
+  | AnswerSectionResponseDto;
