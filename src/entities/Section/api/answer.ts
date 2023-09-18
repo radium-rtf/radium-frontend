3a1f80bcdd0ccc206e158f64e7e02aedfb45f4f6
Danila Petrov <dp200308@gmail.com>
2023-09-18 17:51:47 +0500

    feat: add method and dtos for question section

---
+++ src/entities/Section/api/answer.ts
@@ -0,0 +1,30 @@
+import { authOptions } from '@/entities/Auth';
+import { getServerSession } from 'next-auth';
+import { IErrors } from '@/shared';
+import { AnswerResponseDto } from '..';
+import { AnswerRequestDto } from '../model/answerRequestDto';
+
+export const Answer = async (
+  body: AnswerRequestDto
+): Promise<AnswerResponseDto | IErrors> => {
+  const session = await getServerSession(authOptions);
+  if (!session) return 'Not authenticated';
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL}/answer`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${session.user.accessToken}`,
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    if (response.status !== 200) return 'Not authorized';
+
+    return response.json();
+  } catch (error) {
+    return 'Fetch error';
+  }
+};
--- src/entities/Section/index.ts
+++ src/entities/Section/index.ts
@@ -5,6 +5,7 @@ export type { ChoiceSectionResponseDto } from './model/choiceSectionResponseDto'
 export type { MultiChoiceSectionResponseDto } from './model/multiChoiceSectionResponseDto';
 export type { AnswerSectionResponseDto } from './model/answerSectionResponseDto';
 export type { ShortAnswerSectionResponseDto } from './model/shortAnswerSectionResponseDto';
+export type { AnswerResponseDto } from './model/answerResponseDto';
 
 // types
 export type { AllSectionsResponseDto } from './model/allSectionsResponseDto';
@@ -15,3 +16,6 @@ export { ChoiceSection } from './ui/ChoiceSection';
 export { MultiChoiceSection } from './ui/MultiChoiceSection';
 export { AnswerSection } from './ui/AnswerSection';
 export { ShortAnswerSection } from './ui/ShortAnswerSection';
+
+// methods
+export { Answer } from './api/answer';
