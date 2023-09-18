780164e9e854fadfdf6121f2e1408f0add3ddf98
Danila Petrov <dp200308@gmail.com>
2023-09-18 16:37:47 +0500

    feat: add short answer response dto

--- src/entities/Section/index.ts
+++ src/entities/Section/index.ts
@@ -4,6 +4,7 @@ export type { TextSectionResponseDto } from './model/textSectionResponseDto';
 export type { ChoiceSectionResponseDto } from './model/choiceSectionResponseDto';
 export type { MultiChoiceSectionResponseDto } from './model/multiChoiceSectionResponseDto';
 export type { AnswerSectionResponseDto } from './model/answerSectionResponseDto';
+export type { ShortAnswerSectionResponseDto } from './model/shortAnswerSectionResponseDto';
 
 // types
 export type { AllSectionsResponseDto } from './model/allSectionsResponseDto';
--- src/entities/Section/model/allSectionsResponseDto.ts
+++ src/entities/Section/model/allSectionsResponseDto.ts
@@ -1,10 +1,12 @@
 import { MultiChoiceSectionResponseDto } from '..';
 import { AnswerSectionResponseDto } from './answerSectionResponseDto';
 import { ChoiceSectionResponseDto } from './choiceSectionResponseDto';
+import { ShortAnswerSectionResponseDto } from './shortAnswerSectionResponseDto';
 import { TextSectionResponseDto } from './textSectionResponseDto';
 
 export type AllSectionsResponseDto =
   | TextSectionResponseDto
   | ChoiceSectionResponseDto
   | MultiChoiceSectionResponseDto
-  | AnswerSectionResponseDto;
+  | AnswerSectionResponseDto
+  | ShortAnswerSectionResponseDto;
