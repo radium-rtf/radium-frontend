c114cf03cbca554cedc536ebdc1791d9594c4d7a
Danila Petrov <dp200308@gmail.com>
2023-09-18 16:22:41 +0500

    fix: add name property for text area

--- src/shared/ui/TextArea.tsx
+++ src/shared/ui/TextArea.tsx
@@ -7,6 +7,8 @@ interface TextAreaProps {
   onChange?: ChangeEventHandler<HTMLTextAreaElement>;
   disabled?: boolean;
   placeholder?: string;
+  name: string;
+  value?: string;
 }
 
 export const TextArea: FC<TextAreaProps> = ({
@@ -15,9 +17,13 @@ export const TextArea: FC<TextAreaProps> = ({
   onChange,
   disabled,
   placeholder,
+  name,
+  value,
 }) => {
   return (
     <textarea
+      name={name}
+      value={value}
       placeholder={placeholder}
       onChange={onChange}
       disabled={disabled}
