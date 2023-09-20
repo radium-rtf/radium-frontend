141235abcbb0d93faa15b2dadac63fe254047b06
Danila Petrov <dp200308@gmail.com>
2023-09-20 20:04:23 +0500

    feat: add get page api for rtk

--- src/entities/Page/index.ts
+++ src/entities/Page/index.ts
@@ -3,3 +3,6 @@ export type { PageResponseDto } from './model/PageResponseDto';
 
 // methods
 export { getPageById } from './lib/getPageById';
+
+// hooks
+export { usePageQuery, useLazyPageQuery } from './api/pageApi';
