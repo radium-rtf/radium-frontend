a23d642139861f527be1d0dcb16f2d4f6ef52e92
Danila Petrov <dp200308@gmail.com>
2023-09-21 18:56:47 +0500

    refactor: add anwser option for multi choice section

--- src/entities/Section/ui/MultiChoiceSection.tsx
+++ src/entities/Section/ui/MultiChoiceSection.tsx
@@ -1,29 +1,41 @@
 'use client';
 
-import { Button, Card, Checkbox, Icon } from '@/shared';
-import { FC, FormEventHandler, useRef } from 'react';
-import { MultiChoiceSectionResponseDto } from '..';
+import { Button, Card, Checkbox, Icon, cn } from '@/shared';
+import { FC, FormEventHandler, useState } from 'react';
+import {
+  ChoiceSectionResponseDto,
+  MultiChoiceSectionResponseDto,
+  useAnswerMutation,
+} from '..';
 
 interface IProps {
   data: MultiChoiceSectionResponseDto;
 }
 
 export const MultiChoiceSection: FC<IProps> = ({ data }) => {
-  const form = useRef<HTMLFormElement>(null);
+  const [verdict, setVerdict] = useState<ChoiceSectionResponseDto['verdict']>(
+    data.verdict
+  );
+  const [answer, { isLoading, isError }] = useAnswerMutation();
 
   const onSubmitHandler: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    const formData = new FormData(form.current!);
-    console.log(formData.getAll('answers'));
+    const formData = new FormData(event.currentTarget);
+    answer({
+      id: data.id,
+      multiChoice: {
+        answer: formData.getAll('answers') as string[],
+      },
+    })
+      .unwrap()
+      .then((result) => {
+        result && setVerdict(result.verdict);
+      });
   };
 
   return (
     <Card>
-      <form
-        className='flex flex-col gap-4'
-        ref={form}
-        onSubmit={onSubmitHandler}
-      >
+      <form className='flex flex-col gap-4' onSubmit={onSubmitHandler}>
         <div className='flex items-center gap-4 text-accent-primary-200'>
           <Icon type='question' className='text-inherit' />
           <span className='font-bold text-inherit'>Вопрос</span>
@@ -41,9 +53,29 @@ export const MultiChoiceSection: FC<IProps> = ({ data }) => {
           </ul>
         </main>
         <footer className='flex items-center gap-4 place-self-end'>
-          <span className='text-sm'>{data.maxScore} баллов</span>
+          <div className={cn('flex flex-col gap-2')}>
+            {verdict === 'OK' && (
+              <span className='text-accent-secondary-300'>Верно!</span>
+            )}
+            {verdict === 'WA' && (
+              <span className='text-accent-destructive-300'>Не правильно!</span>
+            )}
+          </div>
+          {!isLoading && !isError && (
+            <span
+              className={cn(
+                'text-sm',
+                verdict === 'OK' && 'text-accent-secondary-300'
+              )}
+            >
+              {verdict === 'OK' && `${data.maxScore} / ${data.maxScore}`}
+              {verdict === 'WA' && `${0} / ${data.maxScore}`}
+              {verdict === '' && `${data.maxScore}`}
+              <span> баллов</span>
+            </span>
+          )}
           <Button type='reset'>Сбросить</Button>
-          <Button type='submit' color='accent'>
+          <Button disabled={isLoading} type='submit' color='accent'>
             Ответить
           </Button>
         </footer>
