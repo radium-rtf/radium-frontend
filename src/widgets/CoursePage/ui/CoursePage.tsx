5fc7bcd217757444932550f4eb4fd41b5ed9ccb6
Danila Petrov <dp200308@gmail.com>
2023-10-03 17:27:26 +0500

    refactor: changed heading font and leading

--- src/widgets/CoursePage/ui/CoursePage.tsx
+++ src/widgets/CoursePage/ui/CoursePage.tsx
@@ -15,7 +15,7 @@ interface IProps {
 export const CoursePage: FC<IProps> = ({ page }) => {
   return (
     <main className='flex w-[45rem] flex-col gap-8'>
-      <h2 className='mx-6 text-5xl font-bold text-accent-primary-200'>
+      <h2 className='mx-6 font-mono text-5xl font-bold leading-[normal] text-accent-primary-200'>
         {page.name}
       </h2>
       {page.sections.map((section) => {
