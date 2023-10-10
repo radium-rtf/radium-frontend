1baac17e3ab9a1ff3756cf1ce2489d642c6097f1
Danila Petrov <dp200308@gmail.com>
2023-10-10 16:45:09 +0500

    feat: add form submit button

---
+++ src/features/FormSubmitButton/ui/FormSubmitButton.tsx
@@ -0,0 +1,32 @@
+import { Button, Icon } from '@/shared';
+import { FC } from 'react';
+
+interface IProps {
+  isLoading: boolean;
+  error?: string;
+  defaultIcon: Icon;
+  text: string;
+}
+
+export const FormSubmitButton: FC<IProps> = ({
+  isLoading,
+  error,
+  defaultIcon,
+  text,
+}) => {
+  return (
+    <Button
+      disabled={isLoading}
+      type='submit'
+      color={error ? 'destructive' : 'accent'}
+    >
+      <Icon
+        className='shrink-0 text-inherit'
+        type={isLoading ? 'loading' : error ? 'alert' : defaultIcon}
+      />
+      <span className='ml-[calc(50%-34px)] -translate-x-1/2'>
+        {isLoading ? 'Загружаем...' : error || text}
+      </span>
+    </Button>
+  );
+};
