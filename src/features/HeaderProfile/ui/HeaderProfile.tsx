c3ee5e0ba30cdd02cfbd8ed9065987cdf5c36dff
Danila Petrov <dp200308@gmail.com>
2023-10-10 16:44:37 +0500

    refactor: removed server side header profile component

--- src/features/HeaderProfile/ui/HeaderProfile.tsx
+++ src/features/HeaderProfile/ui/HeaderProfile.tsx
@@ -1,14 +1,14 @@
+'use client';
 import React from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/entities/Auth';
 import { HeaderMenu } from '@/features/HeaderProfile/ui/HeaderMenu';
+import { useSession } from 'next-auth/react';
 
-export const HeaderProfile = async () => {
-  const session = await getServerSession(authOptions);
+export const HeaderProfile = () => {
+  const { data: session } = useSession();
   return (
     <div className='flex items-center gap-6'>
       {session?.user.name && (
-        <span className='font-mono text-[0.8125rem] leading-tight text-accent-primary-200'>
+        <span className='font-mono text-[0.8125rem] leading-tight  text-accent-primary-200'>
           {session.user.name}
         </span>
       )}
