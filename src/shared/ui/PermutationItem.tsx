9078bd83add5cd58d97d673c10721c1c3382c92e
Danila Petrov <dp200308@gmail.com>
2023-10-23 17:36:30 +0500

    refactor: add hover and drag state for perm item

--- src/shared/ui/PermutationItem.tsx
+++ src/shared/ui/PermutationItem.tsx
@@ -27,6 +27,7 @@ export const PermutationItem: FC<IProps> = ({ value, className, ...props }) => {
     transform: CSS.Transform.toString(transform),
     zIndex: isDragging ? 1 : undefined,
     position: isDragging ? 'relative' : undefined,
+    backgroundColor: isDragging ? '#393E40' : undefined,
   } as CSSProperties;
 
   return (
@@ -37,7 +38,8 @@ export const PermutationItem: FC<IProps> = ({ value, className, ...props }) => {
       {...listeners}
       {...props}
       className={cn(
-        'flex items-center gap-4 rounded-lg border border-transparent px-6 py-2 -outline-offset-1 outline-white transition-colors focus-visible:bg-white/5 focus-visible:outline',
+        'flex items-center gap-4 rounded-lg border border-transparent px-6 py-2',
+        '-outline-offset-1 outline-white transition-colors focus-visible:bg-white/5 focus-visible:outline',
         'hover:border-white/10 hover:bg-white/5',
         className
       )}
